6e9d075e6ddbb61c3f9ff6d0ddcf2b533dbf7945
Abdumutal Abdusamatov <aka.orihero@gmail.com>
2023-07-08 17:00:05 +0500

    refactoring

---
+++ src/components/Modals/Modals.types.ts
@@ -0,0 +1,15 @@
+export type MVisibleType = 'none' | 'i_call_you_now' | 'call_me_later';
+
+export type ModalsState = {
+	type: MVisibleType;
+};
+
+export type ModalsContextType = {
+	show(type: MVisibleType): void;
+	hide(): void;
+	state: ModalsState;
+};
+
+export type ModalsProviderType = {
+	children: any;
+};
---
+++ src/components/Modals/ModalsProvider.tsx
@@ -0,0 +1,42 @@
+import {
+	FC,
+	createContext,
+	useCallback,
+	useContext,
+	useMemo,
+	useState,
+} from 'react';
+import {
+	MVisibleType,
+	ModalsContextType,
+	ModalsProviderType,
+	ModalsState,
+} from './Modals.types';
+import CallMeLaterModal from './CallMeLaterModal';
+import ICallYouNowModal from './ICallYouNowModal';
+
+const ModalsContext = createContext<ModalsContextType | null>(null);
+
+export const ModalsProvider: FC<ModalsProviderType> = ({ children }) => {
+	const [state, updateState] = useState<ModalsState>({} as ModalsState);
+
+	const show = useCallback((type: MVisibleType) => {
+		updateState((oldState) => ({ ...oldState, type }));
+	}, []);
+
+	const hide = useCallback(() => {
+		show('none');
+	}, [show]);
+
+	const value = useMemo(() => ({ show, hide, state }), [hide, show, state]);
+
+	return (
+		<ModalsContext.Provider value={value}>
+			<CallMeLaterModal />
+			<ICallYouNowModal />
+			{children}
+		</ModalsContext.Provider>
+	);
+};
+
+export const useAppModals = () => useContext(ModalsContext);
