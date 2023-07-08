a1cbc14a677dcf467d77c085fdc417ced20f6e78
Abdumutal Abdusamatov <aka.orihero@gmail.com>
2023-07-08 17:50:14 +0500

    refactoring

--- src/components/Procedure/procedure.tsx
+++ src/components/Procedure/procedure.tsx
@@ -1,73 +1,69 @@
-import styled from 'styled-components'
-import BuildingBody from '../BuildingBody/buildingBody'
-import Button from '../Button/button'
-import ProductSizesCarusel from '../ProductSizesCarusel/productSizesCarusel'
-import Text from '../Text/text'
-import styles from "./procedure.module.css"
+import styled from 'styled-components';
+import BuildingBody from '../BuildingBody/buildingBody';
+import Button from '../Button/button';
+import ProductSizesCarusel from '../ProductSizesCarusel/productSizesCarusel';
+import Text from '../Text/text';
+import styles from './procedure.module.css';
 
 const Procedure = () => {
-    return (
-        <Container className={styles.container}>
-            <div className={styles.header}>
-                <Text
-                    className='title'
-                    textType={'middle'}
-                    textSize={'twenty'}
-                    textColor={'black'}
-                    cursor={'Cursor'}
-                    text="Tartiblar"
-                />
-                <Button
-                    className='btnTitle'
-                    textSize={'fourteen'}
-                    btnSize={'small'}
-                    btnColor={'lightPurple'}
-                    textColor={'textPurple'}
-                    borderRadius={'ten'}
-                    title="56"
-                />
-            </div>
-            <div className={styles.body}>
-                <BuildingBody />
-                <ProductSizesCarusel />
-            </div>
-        </Container>
-    )
-}
+	return (
+		<Container className={styles.container}>
+			<div className={styles.header}>
+				<Text
+					className='title'
+					textType={'middle'}
+					textSize={'twenty'}
+					textColor={'black'}
+					cursor={'Cursor'}
+					text='Tartiblar'
+				/>
+				<Button
+					className='btnTitle'
+					textSize={'fourteen'}
+					btnSize={'small'}
+					btnColor={'lightPurple'}
+					textColor={'textPurple'}
+					borderRadius={'ten'}
+					title='56'
+				/>
+			</div>
+			<div className={styles.body}>
+				<BuildingBody />
+				<ProductSizesCarusel />
+			</div>
+		</Container>
+	);
+};
 
 export default Procedure;
 
-
 const Container = styled.div`
+	border-radius: 10px;
 
-    border-radius: 10px;
+	@media (max-width: 992px) {
+	}
 
-    @media (max-width: 992px) {
-        
-    }
+	@media (max-width: 768px) {
+		.title {
+			font-size: 18px;
+		}
 
-    @media (max-width: 768px) {
-        .title {
-            font-size: 18px;
-        }
-        
-        .btnTitle {
-            p {
-                font-size: 14px;
-            }
-        }
-    }
+		.btnTitle {
+			p {
+				font-size: 14px;
+			}
+		}
+	}
 
-    @media (max-width: 576px) {
-        .title {
-            font-size: 16px;
-        }
+	@media (max-width: 576px) {
+		.title {
+			font-size: 16px;
+		}
 
-        .btnTitle {
-            P {
-                font-size: 12px !important;
-            }
-        }
-    }
-
-`
+		.btnTitle {
+			P {
+				font-size: 12px !important;
+			}
+		}
+	}
+`;
