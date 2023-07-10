80c20978d5d9c996d97c6bbd2d0033a146f9ca1b
Shahzodjon Begmamatov <82142092+Begmamatov@users.noreply.github.com>
2023-07-10 11:57:47 +0500

    create ads

--- src/screens/Home/views/Main/main.tsx
+++ src/screens/Home/views/Main/main.tsx
@@ -15,6 +15,8 @@ import { ProductData } from "../../../../constants/productData";
 import { APP_ROUTES } from "../../../../routes/app-router";
 import styles from "./main.module.css";
 import Footer from "../../../../components/Footer/footer";
+import styled from "styled-components";
+import { useRef } from "react";
 
 const Main = () => {
   const { toggle } = useRootStore().visibleStore;
@@ -22,58 +24,65 @@ const Main = () => {
   return (
     <>
       <div className={styles.container}>
-        <div className={styles.banner}>
-          <SwiperComp />
-          <div className={styles.infoBox}>
-            <div className={styles.infoCard}>
-              <RoomsSelect />
-              <PriceFilter label="Narxi, ming $" />
-              <SizeFilter label="Maydoni, m²" />
-              <FinishTimeFilter
-                from={new Date()}
-                to={new Date("Thu Dec 27 2025")}
-                label="Topshirish muddati"
-              />
-              <FloorFilter label="Qavat" />
-              <Button
-                textSize={"fourteen"}
-                btnSize={"filter"}
-                btnColor={"purple"}
-                textColor={"textWhite"}
-                borderRadius={"ten"}
-                title={"306 ta yangi binolarni ko'rish"}
-              />
+          <div className={styles.banner}>
+            <SwiperComp />
+            <div className={styles.infoBox}>
+              <div className={styles.infoCard}>
+                <RoomsSelect />
+                <PriceFilter label="Narxi, ming $" />
+                <SizeFilter label="Maydoni, m²" />
+                <FinishTimeFilter
+                  from={new Date()}
+                  to={new Date("Thu Dec 27 2025")}
+                  label="Topshirish muddati"
+                />
+                <FloorFilter label="Qavat" />
+                <Button
+                  textSize={"fourteen"}
+                  btnSize={"filter"}
+                  btnColor={"purple"}
+                  textColor={"textWhite"}
+                  borderRadius={"ten"}
+                  title={"306 ta yangi binolarni ko'rish"}
+                />
+              </div>
+              <ViewMap />
             </div>
-            <ViewMap />
           </div>
-        </div>
-        <ProductListOfMain />
-        <div className={styles.aboveBox}>
-          <Button
-            textSize={"fourteen"}
-            btnSize={"filter"}
-            btnColor={"purple"}
-            textColor={"textWhite"}
-            borderRadius={"ten"}
-            onPress={() => toggle("filterModal")}
-            title={"306 ta yangi binolarni ko'rish"}
-            style={{
-              width: "100vw",
-              borderRadius: "0",
-              height: "100%",
-            }}
-          />
-        </div>
-        <Footer />
+        <AdsContainerForProducts>
+          <ProductListOfMain classname='productList' />
+          <div className="rightAdsBox"></div>
+        </AdsContainerForProducts>
+          <div className={styles.aboveBox}>
+            <Button
+              textSize={"fourteen"}
+              btnSize={"filter"}
+              btnColor={"purple"}
+              textColor={"textWhite"}
+              borderRadius={"ten"}
+              onPress={() => toggle("filterModal")}
+              title={"306 ta yangi binolarni ko'rish"}
+              style={{
+                width: "100vw",
+                borderRadius: "0",
+                height: "100%",
+              }}
+            />
+          </div>
+          <Footer />
       </div>
     </>
   );
 };
 
-export const ProductListOfMain = () => {
+export const ProductListOfMain = ({
+  classname
+}: {
+  classname?: string;
+}) => {
   const navigation = useNavigate();
   return (
-    <div className={styles.productBox}>
+    <div className={`${styles.productBox} ${classname}`}>
       {ProductData.map((e, index) => {
         return (
           <ProductItem
@@ -104,3 +113,43 @@ export const ProductListOfMain = () => {
 };
 
 export default observer(Main);
+
+
+const AdsContainerForProducts = styled.div`
+  justify-content: space-around;
+  gap: 30px;
+  position: relative;
+  display: grid;
+  grid-template-columns: 1fr 250px;
+  grid-template-areas: 'productList rightAdsBox';
+
+  .productList {
+    grid-area: productList;
+  }
+
+  .rightAdsBox {
+    grid-area: rightAdsBox;
+    width: 250px;
+    height: 500px;
+    background-color: red;
+    position: sticky;
+    top: 50px;
+    margin-top: 20px;
+    margin-bottom: 20px;
+  }
+
+  @media (max-width: 960px) {
+    grid-template-columns: 1fr;
+    grid-template-areas: 'rightAdsBox' 'productList';
+
+    .rightAdsBox {
+      width: 100%;
+      height: 250px;
+      position: relative;
+      top: 0;
+      margin-top: 0;
+      margin-bottom: 0;
+    }
+  }
+
+`
