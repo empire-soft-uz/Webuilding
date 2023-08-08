c7b344b0e92c753de6e513e8a8e8e614b0a922d2
Abdumutal Abdusamatov <aka.orihero@gmail.com>
2023-08-08 16:40:29 +0500

    refactoring

--- src/screens/Home/views/Main/main.tsx
+++ src/screens/Home/views/Main/main.tsx
@@ -219,10 +219,7 @@ export function MultipleSelectChip() {
 		const {
 			target: { value },
 		} = event;
-		setPersonName(
-			// On autofill we get a stringified value.
-			typeof value === 'string' ? value.split(',') : value
-		);
+		setPersonName(typeof value === 'string' ? value.split(',') : value);
 	};
 
 	return (
